72285f9a9ec31afc2882501f5bd646a1e145f92e
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-05 17:47:31 +0530

    added current location provider

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.weatherapp
 
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(!isLocationEnabled()){
+            Toast.makeText(this,
+                "Your location provider is turned off. Please turn it ON"
+                , Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        }else{
+            Toast.makeText(this, "Your location provider is already ON",
+                Toast.LENGTH_SHORT).show()
+        }
     }
+
+    fun isLocationEnabled(): Boolean{
+
+        //this will provide the access to the system location services
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
 }
