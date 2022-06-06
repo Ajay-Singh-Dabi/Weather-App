848812666f21f18b0819d6526f9e02a3dd0fbda4
Ajay-Singh-Dabi <ajaydabi20@gmail.com>
2022-06-06 12:17:44 +0530

    Setting Up UI

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -24,6 +24,7 @@ import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit.*
 
 class MainActivity : AppCompatActivity() {
@@ -139,6 +140,7 @@ class MainActivity : AppCompatActivity() {
                         hideProgressDialog()
 
                         val weatherList: WeatherResponse = response.body()
+                        setupUI(weatherList)
                         Log.i("Response Result","$weatherList")
                     }else{
                         val rc = response.code()
@@ -190,6 +192,24 @@ class MainActivity : AppCompatActivity() {
         if(mProgressDialog != null){
             mProgressDialog!!.dismiss()
         }
+    }
+
+    private fun setupUI(weatherList: WeatherResponse){
+        for(i in weatherList.weather.indices){
+            Log.i("Weather Name",weatherList.weather.toString())
+
+            tv_main.text = weatherList.weather[i].main
+            tv_main_description.text = weatherList.weather[i].description
+            tv_temp.text = weatherList.main.temp.toString() + getUnit(application.resources.configuration.locales.toString())
+        }
+    }
+
+    private fun getUnit(value: String): String?{
+        var value = "°C"
+        if("US" == value || "LR" == value || "MM" == value){
+            value = "°F"
+        }
+        return value
     }
 
 }
